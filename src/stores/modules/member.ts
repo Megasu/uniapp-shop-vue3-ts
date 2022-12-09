586b92101d6b4008baf82e3ee421af438265b4d8
SuJiehao <megasu@qq.com>
2022-12-09 14:26:48 +0800

    feat: 小程序端 pinia 持久化

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -27,6 +27,18 @@ export const useMemberStore = defineStore(
   },
   // TODO: 持久化
   {
-    persist: true,
+    // 网页端配置
+    // persist: true,
+    // 小程序端配置
+    persist: {
+      storage: {
+        getItem(key) {
+          return uni.getStorageSync(key)
+        },
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
+        },
+      },
+    },
   },
 )
