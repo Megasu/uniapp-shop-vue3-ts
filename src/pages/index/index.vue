cf608777eab87ee69e99e4336c68f66d7896bfb2
SuJiehao <megasu@qq.com>
2022-12-22 10:39:02 +0800

    chore: 更新类型声明文件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,9 +9,9 @@
     extra="额外信息"
     thumbnail="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png"
   >
-    <text
-      >这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text
-    >
+    <text>
+      这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。
+    </text>
   </uni-card>
   <view class="index">index</view>
 </template>
