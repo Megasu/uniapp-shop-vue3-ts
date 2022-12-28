f5e836c6b5af68351e05868ff6f7f9dff67b7097
SuJiehao <megasu@qq.com>
2022-12-28 18:27:02 +0800

    feat: 获取轮播图数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+
+// 获取轮播图数据
+const bannerList = ref([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 页面加载
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper />
   <view class="index">index</view>
 </template>
 
---
+++ src/services/home.ts
@@ -0,0 +1,15 @@
+import { http } from '@/utils/http'
+
+/**
+ * 首页-广告区域-小程序
+ * @param distributionSite 广告区域展示位置（投放位置 投放位置，1为首页，2为分类商品页） 默认是1
+ */
+export const getHomeBannerAPI = (distributionSite = 1) => {
+  return http({
+    method: 'GET',
+    url: '/home/banner',
+    data: {
+      distributionSite,
+    },
+  })
+}
