388dd443c36c88d846e95ceb2f5bf913c5578450
SuJiehao <megasu@qq.com>
2023-01-09 10:46:10 +0800

    feat: 猜你喜欢分页条件

--- src/components/XtxGuess.vue
+++ src/components/XtxGuess.vue
@@ -6,19 +6,30 @@ import { onMounted, ref } from 'vue'
 
 // 分页参数
 const pageParams: Required<PageParams> = {
-  page: 1,
+  page: 30,
   pageSize: 10,
 }
 // 猜你喜欢的列表
 const guessList = ref<GuessItem[]>([])
+// 已结束标记
+const finish = ref(false)
 // 获取猜你喜欢数据
 const getHomeGoodsGuessLikeData = async () => {
+  // 退出分页判断
+  if (finish.value === true) {
+    return uni.showToast({ icon: 'none', title: '没有更多数据~' })
+  }
   const res = await getHomeGoodsGuessLikeAPI(pageParams)
   // guessList.value = res.result.items
   // 数组追加
   guessList.value.push(...res.result.items)
-  // 页码累加
-  pageParams.page++
+  // 分页条件
+  if (pageParams.page < res.result.pages) {
+    // 页码累加
+    pageParams.page++
+  } else {
+    finish.value = true
+  }
 }
 // 组件挂载完毕
 onMounted(() => {
@@ -50,7 +61,9 @@ defineExpose({
       </view>
     </navigator>
   </view>
-  <view class="loading-text"> 正在加载... </view>
+  <view class="loading-text">
+    {{ finish ? '没有更多数据~' : '正在加载...' }}
+  </view>
 </template>
 
 <style lang="scss">
