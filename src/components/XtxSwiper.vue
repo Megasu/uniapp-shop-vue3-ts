b8a5770cd8f021d3ad788a522697e9b7405ea7a8
SuJiehao <megasu@qq.com>
2023-01-13 17:12:17 +0800

    chore: 调整轮播图组件样式

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -17,23 +17,10 @@ defineProps<{
 
 <template>
   <view class="carousel">
-    <swiper
-      :circular="true"
-      :autoplay="false"
-      :interval="3000"
-      @change="onChange"
-    >
+    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
       <swiper-item v-for="item in list" :key="item.id">
-        <navigator
-          url="/pages/index/index"
-          hover-class="none"
-          class="navigator"
-        >
-          <image
-            mode="aspectFill"
-            class="image"
-            :src="item.imgUrl"
-          ></image>
+        <navigator url="/pages/index/index" hover-class="none" class="navigator">
+          <image mode="aspectFill" class="image" :src="item.imgUrl"></image>
         </navigator>
       </swiper-item>
     </swiper>
@@ -50,13 +37,9 @@ defineProps<{
 </template>
 
 <style lang="scss">
-:host {
-  display: block;
-  height: 280rpx;
-}
 /* 轮播图 */
 .carousel {
-  height: 100%;
+  height: 280rpx;
   position: relative;
   overflow: hidden;
   transform: translateY(0);
