67856722474d6c7fc3ed90e24af12e4f1d3239fe
SuJiehao <megasu@qq.com>
2023-01-30 21:54:18 +0800

    feat: 热门推荐准备工作

---
+++ src/pages/hot/hot.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+// 热门推荐页 标题和url
+const urlMap = [
+  { type: '1', title: '特惠推荐', url: '/hot/preference' },
+  { type: '2', title: '爆款推荐', url: '/hot/inVogue' },
+  { type: '3', title: '一站买全', url: '/hot/oneStop' },
+  { type: '4', title: '新鲜好物', url: '/hot/new' },
+]
+
+// uniapp 获取页面参数
+const query = defineProps<{
+  type: string
+}>()
+// console.log(query)
+const currUrlMap = urlMap.find((v) => v.type === query.type)
+// 动态设置标题
+uni.setNavigationBarTitle({ title: currUrlMap!.title })
+</script>
+
+<template>
+  <view class="viewport">
+    <!-- 推荐封面图 -->
+    <view class="cover">
+      <image
+        src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-05-20/84abb5b1-8344-49ae-afc1-9cb932f3d593.jpg"
+      ></image>
+    </view>
+    <!-- 推荐选项 -->
+    <view class="tabs">
+      <text class="text active">抢先尝鲜</text>
+      <text class="text">新品预告</text>
+    </view>
+    <!-- 推荐列表 -->
+    <scroll-view scroll-y class="scroll-view">
+      <view class="goods">
+        <navigator
+          hover-class="none"
+          class="navigator"
+          v-for="item in 10"
+          :key="item"
+          :url="`/pages/goods/goods?id=`"
+        >
+          <image
+            class="thumb"
+            src="https://yanxuan-item.nosdn.127.net/5e7864647286c7447eeee7f0025f8c11.png"
+          ></image>
+          <view class="name ellipsis">不含酒精，使用安心爽肤清洁湿巾</view>
+          <view class="price">
+            <text class="symbol">¥</text>
+            <text class="number">29.90</text>
+          </view>
+        </navigator>
+      </view>
+      <view class="loading-text">正在加载...</view>
+    </scroll-view>
+  </view>
+</template>
+
+<style lang="scss">
+page {
+  height: 100%;
+  background-color: #f4f4f4;
+}
+.viewport {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 180rpx 0 0;
+  position: relative;
+}
+.cover {
+  width: 750rpx;
+  height: 225rpx;
+  border-radius: 0 0 40rpx 40rpx;
+  overflow: hidden;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.scroll-view {
+  flex: 1;
+}
+.tabs {
+  display: flex;
+  justify-content: space-evenly;
+  height: 100rpx;
+  line-height: 90rpx;
+  margin: 0 20rpx;
+  font-size: 28rpx;
+  border-radius: 10rpx;
+  box-shadow: 0 4rpx 5rpx rgba(200, 200, 200, 0.3);
+  color: #333;
+  background-color: #fff;
+  position: relative;
+  z-index: 9;
+  .text {
+    margin: 0 20rpx;
+    position: relative;
+  }
+  .active {
+    &::after {
+      content: '';
+      width: 40rpx;
+      height: 4rpx;
+      transform: translate(-50%);
+      background-color: #27ba9b;
+      position: absolute;
+      left: 50%;
+      bottom: 24rpx;
+    }
+  }
+}
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0 20rpx 20rpx;
+  .navigator {
+    width: 345rpx;
+    padding: 20rpx;
+    margin-top: 20rpx;
+    border-radius: 10rpx;
+    background-color: #fff;
+  }
+  .thumb {
+    width: 305rpx;
+    height: 305rpx;
+  }
+  .name {
+    height: 88rpx;
+    font-size: 26rpx;
+  }
+  .price {
+    line-height: 1;
+    color: #cf4444;
+    font-size: 30rpx;
+  }
+  .symbol {
+    font-size: 70%;
+  }
+  .decimal {
+    font-size: 70%;
+  }
+}
+
+.loading-text {
+  text-align: center;
+  font-size: 28rpx;
+  color: #666;
+  padding: 20rpx 0 50rpx;
+}
+</style>
--- src/pages/index/components/HotPanel.vue
+++ src/pages/index/components/HotPanel.vue
@@ -15,7 +15,7 @@ defineProps<{
         <text class="title-text">{{ item.title }}</text>
         <text class="title-desc">{{ item.alt }}</text>
       </view>
-      <navigator hover-class="none" url="/pages/hot/hot" class="cards">
+      <navigator hover-class="none" :url="`/pages/hot/hot?type=${item.type}`" class="cards">
         <image
           v-for="src in item.pictures"
           :key="src"
