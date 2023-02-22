0cd929db70f8aef48c508e12f0ce27848054abc4
SuJiehao <megasu@qq.com>
2023-02-22 21:03:17 +0800

    feat: 小程序快捷登录

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,11 +1,171 @@
 <script setup lang="ts">
-//
+import { postLoginWxMinAPI, postLoginWxMinSimpleAPI } from '@/services/login'
+import { onLoad } from '@dcloudio/uni-app'
+
+// 获取 code 登录凭证
+let code = ''
+onLoad(async () => {
+  const res = await wx.login()
+  code = res.code
+})
+
+// 获取用户手机号码（企业中写法）
+const onGetphonenumber: UniHelper.ButtonOnGetphonenumber = async (ev) => {
+  const encryptedData = ev.detail!.encryptedData!
+  const iv = ev.detail!.iv!
+  const res = await postLoginWxMinAPI({ code, encryptedData, iv })
+  console.log(res)
+}
+
+// 模拟手机号码快捷登录（开发练习）
+const onGetphonenumberSimple = async () => {
+  const res = await postLoginWxMinSimpleAPI('13123456789')
+  console.log(res)
+  uni.showToast({ icon: 'none', title: '登录成功' })
+}
 </script>
 
 <template>
-  <view class="login">login</view>
+  <view class="viewport">
+    <view class="logo">
+      <image
+        src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/images/logo_icon.png"
+      ></image>
+    </view>
+    <view class="login">
+      <button class="button phone" open-type="getPhoneNumber" @getphonenumber="onGetphonenumber">
+        <text class="icon icon-phone"></text>
+        手机号快捷登录
+      </button>
+      <view class="extra">
+        <view class="caption">
+          <text>其他登录方式</text>
+        </view>
+        <view class="options">
+          <button @tap="onGetphonenumberSimple">
+            <text class="icon icon-phone">模拟快捷登录</text>
+          </button>
+        </view>
+      </view>
+      <view class="tips">登录/注册即视为你同意《服务条款》和《小兔鲜儿隐私协议》</view>
+    </view>
+  </view>
 </template>
 
 <style lang="scss">
-//
+page {
+  height: 100%;
+}
+
+.viewport {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 20rpx 40rpx;
+}
+
+.logo {
+  flex: 1;
+  text-align: center;
+  image {
+    width: 220rpx;
+    height: 220rpx;
+    margin-top: 15vh;
+  }
+}
+
+.login {
+  display: flex;
+  flex-direction: column;
+  height: 60vh;
+  padding: 40rpx 20rpx 20rpx;
+
+  .button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 80rpx;
+    font-size: 28rpx;
+    border-radius: 72rpx;
+    color: #fff;
+    .icon {
+      font-size: 40rpx;
+      margin-right: 6rpx;
+    }
+  }
+
+  .phone {
+    background-color: #28bb9c;
+  }
+
+  .wechat {
+    background-color: #06c05f;
+  }
+
+  .extra {
+    flex: 1;
+    padding: 70rpx 70rpx 0;
+    .caption {
+      width: 440rpx;
+      line-height: 1;
+      border-top: 1rpx solid #ddd;
+      font-size: 26rpx;
+      color: #999;
+      position: relative;
+      text {
+        transform: translate(-40%);
+        background-color: #fff;
+        position: absolute;
+        top: -12rpx;
+        left: 50%;
+      }
+    }
+
+    .options {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 70rpx;
+      button {
+        padding: 0;
+        background-color: transparent;
+      }
+    }
+
+    .icon {
+      font-size: 24rpx;
+      color: #444;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      &::before {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 80rpx;
+        height: 80rpx;
+        margin-bottom: 6rpx;
+        font-size: 40rpx;
+        border: 1rpx solid #444;
+        border-radius: 50%;
+      }
+    }
+    .icon-weixin::before {
+      border-color: #06c05f;
+      color: #06c05f;
+    }
+  }
+}
+
+.tips {
+  position: absolute;
+  bottom: 80rpx;
+  left: 20rpx;
+  right: 20rpx;
+  font-size: 22rpx;
+  color: #999;
+  text-align: center;
+}
 </style>
---
+++ src/services/login.ts
@@ -0,0 +1,32 @@
+import { http } from '@/utils/http'
+
+type LoginParams = {
+  code: string
+  encryptedData: string
+  iv: string
+}
+/**
+ * 小程序登录
+ * @param data 请求参数
+ */
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http({
+    method: 'POST',
+    url: '/login/wxMin',
+    data,
+  })
+}
+
+/**
+ * 小程序登录_内测版
+ * @param phoneNumber 模拟手机号码
+ */
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber,
+    },
+  })
+}
