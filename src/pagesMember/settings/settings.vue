1ddb973cdb921f85cef29c58921d7a88194e4a63
SuJiehao <megasu@qq.com>
2023-02-28 21:08:05 +0800

    feat: 个人中心-退出登录

--- src/pagesMember/settings/settings.vue
+++ src/pagesMember/settings/settings.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-//
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+// 退出登录
+const onLogout = () => {
+  // 模态弹窗
+  uni.showModal({
+    content: '是否退出登录？',
+    success: (res) => {
+      if (res.confirm) {
+        // 清理用户信息
+        memberStore.clearProfile()
+        // 返回上一页
+        uni.navigateBack()
+      }
+    },
+  })
+}
 </script>
 
 <template>
   <view class="viewport">
     <!-- 列表1 -->
-    <view class="list" v-if="true">
+    <view class="list" v-if="memberStore.profile">
       <navigator url="./address/address" hover-class="none" class="item arrow">
         我的收货地址
       </navigator>
@@ -21,8 +38,8 @@
       <navigator hover-class="none" class="item arrow" url=" ">关于小兔鲜儿</navigator>
     </view>
     <!-- 操作按钮 -->
-    <view class="action">
-      <view class="button">退出登录</view>
+    <view class="action" v-if="memberStore.profile">
+      <view @tap="onLogout" class="button">退出登录</view>
     </view>
   </view>
 </template>
