f9bb19191b1951b7d169b4b62fb4def4a9fb5ed6
SuJiehao <megasu@qq.com>
2023-03-01 09:28:19 +0800

    chore: update

--- src/pagesMember/settings/settings.vue
+++ src/pagesMember/settings/settings.vue
@@ -23,7 +23,7 @@ const onLogout = () => {
   <view class="viewport">
     <!-- 列表1 -->
     <view class="list" v-if="memberStore.profile">
-      <navigator url="./address/address" hover-class="none" class="item arrow">
+      <navigator url="/pagesMember/address/address" hover-class="none" class="item arrow">
         我的收货地址
       </navigator>
     </view>
