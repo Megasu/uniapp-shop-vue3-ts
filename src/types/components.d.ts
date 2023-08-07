0ad3f83f76d230e124f6406448f334a2f7bafdaf
苏杰豪 <Megasu@qq.com>
2023-08-07 10:21:56 +0800

    feat: 全局组件类型-最新写法

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,7 +1,7 @@
 import XtxSwiper from '@/components/XtxSwiper.vue'
 import XtxGuess from '@/components/XtxGuess.vue'
 
-declare module '@vue/runtime-core' {
+declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
     XtxGuess: typeof XtxGuess
@@ -10,3 +10,4 @@ declare module '@vue/runtime-core' {
 
 // 组件实例类型
 export type XtxGuessInstance = InstanceType<typeof XtxGuess>
+export type XtxSwiperInstance = InstanceType<typeof XtxSwiper>
