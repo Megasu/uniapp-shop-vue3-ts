53abe3444941bc40a4e1a3d8ebbc0e75756463a6
苏杰豪 <Megasu@qq.com>
2023-08-10 22:03:40 +0800

    fix: 修复购物车触底分页加载

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -5,3 +5,9 @@ import CartMain from './components/CartMain.vue'
 <template>
   <CartMain />
 </template>
+
+<style lang="scss">
+page {
+  height: 100%;
+}
+</style>
--- src/pages/cart/cart2.vue
+++ src/pages/cart/cart2.vue
@@ -6,3 +6,9 @@ import CartMain from './components/CartMain.vue'
   <!-- 适配底部安全区 -->
   <CartMain safe-area-inset-bottom />
 </template>
+
+<style lang="scss">
+page {
+  height: 100%;
+}
+</style>
